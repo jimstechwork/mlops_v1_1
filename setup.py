29f3cff4db41f9f343e45afa9e9464d401026684
jimstechwork <jimstechwork@gmail.com>
2023-07-17 12:37:49 +0530

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A test project to test the MLOPS lifecyclce',
+    author='Jimreeves Ch',
+    license='MIT',
+)
